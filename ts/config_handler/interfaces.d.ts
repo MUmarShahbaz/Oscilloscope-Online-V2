65fa6076f4cd73ffdf13ebd1619130a2428dd9ed
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-07-21 00:00:01 +0500

    Created Structure for the overall config

---
+++ ts/config_handler/interfaces.d.ts
@@ -0,0 +1,48 @@
+namespace CONFIG_HANDLER {
+	type color = `#${string}`;
+	type time_format = "ms" | "s.ms" | "m:s" | "h:m:s" | "d-h:m";
+
+	interface CONFIG {
+		serial: {
+			break: string;
+			mcu_commands: {
+				cls: string;
+				csv: string;
+				png: string;
+				json: string;
+			};
+		};
+		chart: {
+			title: string;
+			x: {
+				type: "linear";
+				title: string;
+				min: number;
+				max: number;
+			} 
+			| {
+				type: "time";
+				title: string;
+				manual: boolean;
+				format: time_format;
+				max: number;
+			};
+			y: {
+				type: 'linear-auto';
+				title: string;
+			}
+			| {
+				type: 'linear-manual';
+				title: string;
+				min: number;
+				max: number;
+			} 
+			| {
+				type: 'log';
+				title: string;
+				base: number;
+			};
+		};
+		datasets: Array<{ label: string; color: color; }>;
+	}
+}
