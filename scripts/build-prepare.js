a2911540e2a3933aa51c5b9c26eed718ad100e0f
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-07-23 13:28:33 +0500

    Added config_handler to Setup

--- scripts/build-prepare.js
+++ scripts/build-prepare.js
@@ -10,7 +10,7 @@ let config = yaml.parse(fs.readFileSync('./jekyll/template_config.yml', 'utf8'))
 // Determine which assets directory to use
 const mode = process.argv[2] || '--dev';
 const commit_hash = execSync('git log origin/assets -1 --format=%H').toString().trim();
-const assets_dir = mode === '--prod' ? `https://cdn.jsdelivr.net/gh/MUmarShahbaz/Oscilloscope-Online-V2@${commit_hash}` : 'assets';
+const assets_dir = mode === '--prod' ? `https://cdn.jsdelivr.net/gh/MUmarShahbaz/Oscilloscope-Online-V2@${commit_hash}` : '/assets';
 
 if (mode === '--dev') {
     // Use local assets and host appropriate to http-server
@@ -25,7 +25,7 @@ if (mode === '--dev') {
 
 // Update directories
 config.logo = `${assets_dir}/img/icon.svg`;
-config.dynamic_assets.source.asset_dir = `${assets_dir}`;
+config.dynamic_assets.source.base = `${assets_dir}`;
 
 // Save as _config.yml
 fs.writeFileSync('./jekyll/_config.yml', yaml.stringify(config), 'utf8');
