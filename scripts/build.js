fb5ad67ed320c493ceace450f7837e18067a239b
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-07-29 19:51:17 +0500

    Updated Build-Host-Clean process and js files

--- scripts/build.js
+++ scripts/build.js
@@ -1,18 +1,27 @@
 const { execSync } = require('child_process');
+const { create_child, quit_all, check_package } = require('./sub_mgmt');;
 const fs = require('fs');
 const yaml = require('yaml');
 const path = require('path');
 const os = require('os');
 
+let children = [];
+process.on('SIGINT', () => quit_all(children));
+
 // Read template config
+console.log('Reading template config');
 let config = yaml.parse(fs.readFileSync('./jekyll/template_config.yml', 'utf8'));
 
 // Determine which assets directory to use
 const mode = process.argv[2] || '--dev';
+console.log(`${mode === '--dev' ? 'Development' : 'Production'} mode`);
+
 const commit_hash = execSync('git log origin/assets -1 --format=%H').toString().trim();
 const assets_dir = mode === '--prod' ? `https://cdn.jsdelivr.net/gh/MUmarShahbaz/Oscilloscope-Online-V2@${commit_hash}` : '/assets';
+console.log(`Assets: ${assets_dir}`);
 
 if (mode === '--dev') {
+    console.log('Updating URLs to use private IP Address');
     // Use local assets and host appropriate to http-server
     const interface = os.networkInterfaces();
     const ip = Object.values(interface)
@@ -20,6 +29,9 @@ if (mode === '--dev') {
         .find(i => i.family === 'IPv4' && !i.internal)?.address;
     config.url = `http://${ip}:8080`;
     config.baseurl = null;
+
+    // Cloning assets internally
+    console.log('Copying assets to ./jekyll');
     fs.cpSync(path.resolve('./assets'), path.resolve('./jekyll/assets'), { recursive: true });
 }
 
@@ -29,3 +41,12 @@ config.dynamic_assets.source.base = `${assets_dir}`;
 
 // Save as _config.yml
 fs.writeFileSync('./jekyll/_config.yml', yaml.stringify(config), 'utf8');
+console.log('Done');
+
+const bundler = check_package('bundle -v');
+if (bundler.present) {
+    create_child('Jekyll => Build', 'bundle exec jekyll build', './jekyll');
+} else {
+    console.log('Bundler was not detected! Could not continue.');
+    console.log(bundler.error);
+}
--- scripts/build_cleanup.js
+++ scripts/build_cleanup.js
@@ -4,11 +4,13 @@ const path = require('path');
 // Delete delete _config.yml
 const config_file = path.resolve('./jekyll/_config.yml');
 if (fs.existsSync(config_file)) {
+  console.log('Deleting session config file');
   fs.unlinkSync(config_file);
 }
 
 // Delete jekyll assets folder
 const assets_folder = path.resolve('./jekyll/assets');
 if (fs.existsSync(assets_folder)) {
+  console.log('Removing copies of assets');
   fs.rmSync(assets_folder, { recursive: true, force: true });
 }
