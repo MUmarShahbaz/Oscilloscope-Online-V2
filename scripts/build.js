8d94e31d6dced73b7c26886df24d296ce615554f
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-07-29 20:37:02 +0500

    Added Live-Host

--- scripts/build.js
+++ scripts/build.js
@@ -14,13 +14,13 @@ let config = yaml.parse(fs.readFileSync('./jekyll/template_config.yml', 'utf8'))
 
 // Determine which assets directory to use
 const mode = process.argv[2] || '--dev';
-console.log(`${mode === '--dev' ? 'Development' : 'Production'} mode`);
+console.log(`${mode === '--prod' ? 'Production' : 'Development'} mode`);
 
 const commit_hash = execSync('git log origin/assets -1 --format=%H').toString().trim();
 const assets_dir = mode === '--prod' ? `https://cdn.jsdelivr.net/gh/MUmarShahbaz/Oscilloscope-Online-V2@${commit_hash}` : '/assets';
 console.log(`Assets: ${assets_dir}`);
 
-if (mode === '--dev') {
+if (mode === '--dev' || mode === '--prep') {
     console.log('Updating URLs to use private IP Address');
     // Use local assets and host appropriate to http-server
     const interface = os.networkInterfaces();
@@ -42,11 +42,12 @@ config.dynamic_assets.source.base = `${assets_dir}`;
 // Save as _config.yml
 fs.writeFileSync('./jekyll/_config.yml', yaml.stringify(config), 'utf8');
 console.log('Done');
-
-const bundler = check_package('bundle -v');
-if (bundler.present) {
-    create_child('Jekyll => Build', 'bundle exec jekyll build', './jekyll');
-} else {
-    console.log('Bundler was not detected! Could not continue.');
-    console.log(bundler.error);
+if (!mode === '--prep') {
+    const bundler = check_package('bundle -v');
+    if (bundler.present) {
+        create_child('Jekyll => Build', 'bundle exec jekyll build', './jekyll');
+    } else {
+        console.log('Bundler was not detected! Could not continue.');
+        console.log(bundler.error);
+    }
 }
---
+++ scripts/live_host.js
@@ -0,0 +1,34 @@
+const { execSync } = require('child_process');
+const { create_child, quit_all, check_package } = require('./sub_mgmt');
+
+let children = [];
+
+process.on('SIGINT', () => {
+    console.log('Cleaning up');
+    execSync('npm run clean')
+    quit_all(children);
+});
+
+const http = check_package('npx http-server --version');
+const bundle = check_package('bundle -v');
+
+if (http.present) {
+    console.log('✅ http-server is available.');
+} else {
+    console.log('❌ http-server is not available in this directory.');
+    console.log('try `npm run dependencies`');
+    console.error(http.error);
+    process.exit(1);
+}
+
+if (bundle.present) {
+    console.log('✅ bundle is available.');
+} else {
+    console.log('❌ bundle is not available in this directory.');
+    console.log('try `npm run dependencies`');
+    console.error(bundle.error);
+    process.exit(1);
+}
+
+create_child('http-server', 'http-server -p 8080', './jekyll/_site', 18);
+create_child('bundler => jekyll', 'bundle exec jekyll build --watch', './jekyll' , 18);
